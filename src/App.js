1d930ae3f67a6d4d779d875310cf69fba9707e16
jeff <jeffhogg86@gmail.com>
2021-02-25 23:09:52 -0800

    edit text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hey ya</p>
+        <p>Hey ya, ya</p>
         <a
           className="App-link"
           href="https://reactjs.org"
